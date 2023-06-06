54711753cf165d54b55ade7424d09bf820f5dae9
Curzsu <86864691+Curzsu@users.noreply.github.com>
2023-06-06 23:10:00 +0800

    优化龙卷风音效，修改地板bug

--- Assets/Scripts/Load2.cs
+++ Assets/Scripts/Load2.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class Load2 : MonoBehaviour
 {
     void Start()
     {
         // 延迟3秒后执行 LoadScene 函数
         Invoke("LoadScene", 5f);
-
     }
 
     void LoadScene()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("LoadingScreen2");
     }
 }
